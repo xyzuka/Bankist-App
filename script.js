96e07c4aa3cdbd745e05ec11bfba8e5420c0981e
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-30 17:15:01 +1100

    Updating movement dates
    1. Refactored code to display how many days ago a deposit or withdraw was performed -> compressed the variables and function into a new function to be called
    2. Kept the displayDate variable inside the displayMovements function since the html template literal is referencing it
    3. Created a function to calculate the difference between two dates, and ran it
    4. Added if statements to determine if the deposit or withdrawal was done x days ago

--- script.js
+++ script.js
@@ -40,12 +40,12 @@ const account1 = {
     '2019-12-23T07:42:02.383Z',
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2021-05-08T14:11:59.604Z',
+    '2022-03-25T17:01:17.194Z',
+    '2022-03-28T23:36:17.929Z',
+    '2022-03-30T10:51:36.790Z',
   ],
-  currency: 'EUR',
+  currenc2: 'EUR',
   locale: 'pt-PT', // de-DE
 };
 
@@ -59,11 +59,11 @@ const account2 = {
     '2019-11-01T13:15:33.035Z',
     '2019-11-30T09:48:16.867Z',
     '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2021-01-25T14:18:46.235Z',
+    '2021-02-05T16:33:06.386Z',
+    '2022-02-10T14:43:26.374Z',
+    '2022-03-25T18:49:59.371Z',
+    '2022-03-26T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -73,6 +73,24 @@ const accounts = [account1, account2];
 
 /////////////////////////////////////////////////
 // FUNCTIONS
+
+// Format movement date function
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+  // 1000 mil in a sec => 60sec in a min =>  60min in a hr => 24hrs in a day
+
+  const daysPassed = Math.round(calcDaysPassed(new Date(), date));
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  const day = `${date.getDate()}`.padStart(2, '0');
+  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
 
 // Updates DOM for movements of deposits and withdrawals
 const displayMovements = function (acc, sort = false) {
@@ -86,10 +104,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, '0');
-    const month = `${date.getMonth() + 1}`.padStart(2, '0');
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
