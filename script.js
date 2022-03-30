70199d1a2af180b510ac2c488bade4468914dd46
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-30 16:32:47 +1100

    Added padding
    Added padding to the welcome time (hrs and mins)

--- script.js
+++ script.js
@@ -227,8 +227,8 @@ btnLogin.addEventListener('click', function (e) {
   const day = `${now.getDate()}`.padStart(2, '0');
   const month = `${now.getMonth() + 1}`.padStart(2, '0');
   const year = now.getFullYear();
-  const hour = now.getHours();
-  const minute = now.getMinutes();
+  const hour = `${now.getHours()}`.padStart(2, '0');
+  const minute = `${now.getMinutes()}`.padStart(2, '0');
 
   labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minute}`;
 
