f5b944fa03ea55c1e7596e8af5017796cd3093db
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-31 14:07:14 +1100

    Fixed Sorting
    Sort function was not working because the displayMovements function being run in the event listener was taking in the movements array and not the current user (since the displayMovements parameters got changed)

--- script.js
+++ script.js
@@ -350,7 +350,7 @@ let sorted = false;
 
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
